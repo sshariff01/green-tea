49508dacf96ef8462548c71e7eb6346745b8514a
sshariff01 <shoabe.shariff@gmail.com>
2013-01-20 00:06:56 -0500

    Refactor

--- src/Solution.java
+++ src/Solution.java
@@ -7,13 +7,10 @@ import java.util.Collections;
 import java.util.Comparator;
 
 public class Solution {
-
-//	private static final int N_MAX = 10000;
 	private int N, W, H;
 	private int[][] eventsFromInput;
 	private int[][] storyEvents;
 	private int[][] reloadEvents;
-	private int[][] binaryChart;
 
 	public Solution() {
 		// TODO Auto-generated constructor stub
@@ -52,7 +49,6 @@ public class Solution {
 		}
 
 		isolateEvents(this.eventsFromInput, numReloads, numStories);
-		System.out.println("The isolateEvents method ran without blowing up");
 	}
 
 	private void isolateEvents(int[][] events, int numReloads, int numStories) {
@@ -87,15 +83,7 @@ public class Solution {
 		int[][] targetStories = new int[this.W - 1][3];
 		int numTargetedStories = 0;
 		int timeOfReload;
-		int totalScoreOfAllTargetedStories = 0;
-		int totalSizeOfAllTargetedStories = 0;
 		int[] outputData;
-		StringBuilder targetedStoryIDs = new StringBuilder(); // Use
-																// StringBuilder
-																// to aggregate
-																// IDs of all
-																// Targeted
-																// stories
 		boolean flag = false; // TODO: Refine usage of flag
 
 		for (int i = 0; i < this.reloadEvents.length; i++) { // Begin by
@@ -124,25 +112,27 @@ public class Solution {
 						targetStories[numTargetedStories][k] = this.storyEvents[j][k];
 					}
 					targetStories[numTargetedStories][0] = j + 1;
-//					targetedStoryIDs.append(" ");
-//					targetedStoryIDs
-//							.append(targetStories[numTargetedStories][0]);
-//					totalScoreOfAllTargetedStories += targetStories[numTargetedStories][1];
-//					totalSizeOfAllTargetedStories += targetStories[numTargetedStories][2];
 					numTargetedStories++;
 				} else if (flag) { // Break out of the for-loop because we've
 									// gotten all the stories we need to target
 					break;
 				}
 			}
+
 			// For reload event at i, we now need to calculate the optimized
 			// score and output the appropriate information,
 			// and then carry on to the next iteration of the for-loop (next
 			// reload event)
+			outputData = createAndExtractBinaryChart(targetStories,
+					numTargetedStories);
 
-			outputData = createBinaryChart(targetStories, numTargetedStories);
-
-			System.out.println(outputData[numTargetedStories] + " " + getNumberOfStories(outputData, numTargetedStories) + " " + getStoryIds(outputData, numTargetedStories, targetStories));
+			System.out
+					.println(outputData[numTargetedStories]
+							+ " "
+							+ getNumberOfStories(outputData, numTargetedStories)
+							+ " "
+							+ getStoryIds(outputData, numTargetedStories,
+									targetStories));
 
 			targetStories = new int[this.W - 1][3];
 			numTargetedStories = 0;
@@ -151,89 +141,93 @@ public class Solution {
 		}
 	}
 
-	private int[] createBinaryChart(int[][] targetStories, int numStories){
-		int[][] binaryChart = new int[(int) Math.pow(2, numStories)][numStories+1];
+	private int[] createAndExtractBinaryChart(int[][] targetStories,
+			int numStories) {
+		int[][] binaryChart = new int[(int) Math.pow(2, numStories)][numStories + 1];
 		String binaryRep;
 		StringBuffer reversedBinary;
 		int combinationScore;
-		
-		for(int i=0; i<Math.pow(2, numStories); i++){
+
+		for (int i = 0; i < Math.pow(2, numStories); i++) {
 			binaryRep = Integer.toBinaryString(i);
 			reversedBinary = new StringBuffer();
 			reversedBinary.append(binaryRep).reverse();
-//			System.out.println(reversedBinary);
 			combinationScore = 0;
-			for(int j=numStories-1; j>=0; j--){
-				if(j >= reversedBinary.length()){
+			for (int j = numStories - 1; j >= 0; j--) {
+				if (j >= reversedBinary.length()) {
 					binaryChart[i][j] = 0;
 				} else {
-					if(reversedBinary.charAt(j) == 48){
+					if (reversedBinary.charAt(j) == 48) {
 						binaryChart[i][j] = 0;
-					}
-					else if(reversedBinary.charAt(j) == 49){
+					} else if (reversedBinary.charAt(j) == 49) {
 						binaryChart[i][j] = 1;
 					}
 				}
 			}
-			for(int k=0; k < numStories; k++){
-				if(binaryChart[i][k] == 1)
+			for (int k = 0; k < numStories; k++) {
+				if (binaryChart[i][k] == 1)
 					combinationScore += targetStories[k][1];
 			}
 			binaryChart[i][numStories] = combinationScore;
 		}
-		int[] outputData = new int[numStories+1];
-		outputData = getDataFromBinaryChart(binaryChart, numStories, targetStories);
-		
+		int[] outputData = new int[numStories + 1];
+		outputData = getDataFromBinaryChart(binaryChart, numStories,
+				targetStories);
+
 		return outputData;
 	}
-	
-	private int[] getDataFromBinaryChart(int[][] binaryChart, int numStories, int[][] targetStories){
-		// First we need to sort the two-dimensional array, but instead we sort their scores and use it to find the appropriate row in the binaryChart
+
+	private int[] getDataFromBinaryChart(int[][] binaryChart, int numStories,
+			int[][] targetStories) {
+		// First we need to sort the two-dimensional array, but instead we sort
+		// their scores and use it to find the appropriate row in the
+		// binaryChart
 		int val = 0;
-		int[] scores = new int[(int) (Math.pow(2, numStories)-1)];
-		for(int i=(int) (Math.pow(2, numStories)-1); i>0; i--){
-			scores[i-1] = binaryChart[i][numStories];
+		int[] scores = new int[(int) (Math.pow(2, numStories) - 1)];
+		for (int i = (int) (Math.pow(2, numStories) - 1); i > 0; i--) {
+			scores[i - 1] = binaryChart[i][numStories];
 		}
 		Arrays.sort(scores);
-		
+
 		int combinationSize;
-		int[] outputData = new int[numStories+1];
-		for(int j=scores.length; j>0; j--){
-			for(int l=scores.length; l>0; l--){
-				if(scores[j-1] == binaryChart[l][numStories]){
+		int[] outputData = new int[numStories + 1];
+		for (int j = scores.length; j > 0; j--) {
+			for (int l = scores.length; l > 0; l--) {
+				if (scores[j - 1] == binaryChart[l][numStories]) {
 					val = l;
 				}
 			}
-			combinationSize=0;
-			for(int k=0; k<numStories; k++){
-				if (binaryChart[val][k] == 1){
+			combinationSize = 0;
+			for (int k = 0; k < numStories; k++) {
+				if (binaryChart[val][k] == 1) {
 					combinationSize += targetStories[k][2];
 				}
 			}
-			if (combinationSize <= this.H){
+			if (combinationSize <= this.H) {
 				outputData = binaryChart[val];
 				return outputData;
 			}
 		}
-		
+
 		return outputData;
 	}
 
 	private int getNumberOfStories(int[] outputData, int n) {
 		int numberOfStories = 0;
-		for(int i=0; i<n; i++){
-			if(outputData[i] == 1)
+		for (int i = 0; i < n; i++) {
+			if (outputData[i] == 1)
 				numberOfStories++;
 		}
 		return numberOfStories;
 	}
-	
+
 	@SuppressWarnings("null")
-	private StringBuilder getStoryIds(int[] outputData, int n, int[][] targetStories) {
+	private StringBuilder getStoryIds(int[] outputData, int n,
+			int[][] targetStories) {
 		StringBuilder storyIds = new StringBuilder();
-		for(int i=0; i<n; i++){
-			if(outputData[i] == 1){
-				if(storyIds == null){
+		for (int i = 0; i < n; i++) {
+			if (outputData[i] == 1) {
+				if (storyIds == null) {
 					storyIds.append(targetStories[i][0]);
 				} else {
 					storyIds.append(" ");
